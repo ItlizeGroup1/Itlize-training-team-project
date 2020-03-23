f0091aed49ec68b169ca909ec3df4aea8a7247ae
liu Wenxuan <woshitc8535@163.com>
2020-03-23 17:54:50 -0400

    Creat spring project

---
+++ src/test/java/com/itlize/Korera/KoreraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itlize.Korera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
