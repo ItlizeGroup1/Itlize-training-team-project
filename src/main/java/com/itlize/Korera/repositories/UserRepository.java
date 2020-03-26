d0f2990f97fedb08411b264a5f6bd34a5511a3e3
shabbyme <625747835@qq.com>
2020-03-26 04:31:18 -0400

    Change UserRepository since the Id of User model changed

--- src/main/java/com/itlize/Korera/repositories/UserRepository.java
+++ src/main/java/com/itlize/Korera/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package com.itlize.Korera.repositories;
 import com.itlize.Korera.dbModels.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, String> {
 
 }
