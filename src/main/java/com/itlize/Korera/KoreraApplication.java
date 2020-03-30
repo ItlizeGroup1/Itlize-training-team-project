967a3d751771be792abe433a89061e2a25883e78
liu Wenxuan <woshitc8535@163.com>
2020-03-30 11:26:29 -0400

    fix createDate annotation

--- src/main/java/com/itlize/Korera/KoreraApplication.java
+++ src/main/java/com/itlize/Korera/KoreraApplication.java
@@ -2,8 +2,10 @@ package com.itlize.Korera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KoreraApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/itlize/Korera/dbModels/User.java
+++ src/main/java/com/itlize/Korera/dbModels/User.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.*;
@@ -16,7 +17,7 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 
-
+@EntityListeners(AuditingEntityListener.class)
 @PropertySource(value = {"classpath:users.properties"})
 @Component
 @ConfigurationProperties(prefix = "user")
